65b80b46f7731bdf0d6c338b62343105283e73a7
tanay-pingalkar <tanaydpingalkar@gmail.com>
2020-11-26 12:24:41 +0500

    Updates

---
+++ precache-manifest.9c61773c7f0592a0eec2510c025e3440.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0942f8d52201abe917a8db94e4f953cc",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e7a590547ca07f6cd8f2",
+    "url": "/weather-app/static/css/main.2580940d.chunk.css"
+  },
+  {
+    "revision": "e1fcbf16f6024373e757",
+    "url": "/weather-app/static/js/2.e6579e2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.e6579e2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a590547ca07f6cd8f2",
+    "url": "/weather-app/static/js/main.e1ba40d8.chunk.js"
+  },
+  {
+    "revision": "7bc5279d7f36ff585943",
+    "url": "/weather-app/static/js/runtime-main.25231bf1.js"
+  },
+  {
+    "revision": "b18af339f65e7c20f3d03ecdd0ce54df",
+    "url": "/weather-app/static/media/background.b18af339.jpg"
+  }
+]);
